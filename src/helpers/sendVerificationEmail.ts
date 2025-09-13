78b43ebca7330b5053cc4a8a93450d6c8736ed97
AyushSahrawat47 <ayushsahrawat47@gmail.com>
2025-09-13 08:24:26 +0530

    resend setup done

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,28 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "../types/apiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "Productivity App - Verify Your Email",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    if(error){
+        console.error("Resend error:", error);
+        return { success: false, message: "Failed to send verification email" };
+    } else {
+        return { success: true, message: "Verification email sent successfully" };
+    }
+  } catch (err) {
+    console.error("Error sending verification email:", err);
+    return { success: false, message: "Failed to send verification email" };
+  }
+}
